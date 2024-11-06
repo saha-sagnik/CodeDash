5a72d59637340c5432c984f5b1a1d60491744b64
Sagnik Saha <sagnik12347@gmail.com>
2024-11-07 02:40:49 +0530

    new user data created

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,12 @@
 "use server"
 
 import * as z from "zod";
+import bcrypt, { hash } from "bcrypt"
 
 import { RegisterSchema } from "@/schema";
+import { db } from "@/lib/db";
+import { error } from "console";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values:z.infer<typeof RegisterSchema>)=>{
     const validateFields = RegisterSchema.safeParse(values);
@@ -12,6 +16,26 @@ export const register = async (values:z.infer<typeof RegisterSchema>)=>{
             error: "Invalid fields!",
         }
     }
+
+    const {email,password,name} = validateFields.data;
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const exisitngUser = await getUserByEmail(email);
+
+    if(exisitngUser)
+        return{
+    error:"Email already in use!"
+}
+
+    await db.user.create({
+        data:{
+            email,
+            name,
+            password: hashedPassword,
+        },
+    });
+
+    //Tode: send verification token number
 
     return {
         success: "Email sent!"
